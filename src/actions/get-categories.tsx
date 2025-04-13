e9d219ff0a85240dc90137609f08a45e4394c557
cuong <tuilanguyencuong@gmail.com>
2025-04-13 12:22:56 +0700

    edit env

--- src/actions/get-categories.tsx
+++ src/actions/get-categories.tsx
@@ -1,23 +1,17 @@
-
-
 import { Category } from "@/types/ProjectInterface";
 
-const URL = `${process.env.NEXT_PUBLIC_API}/categories`
+const URL = `${process.env.NEXT_PUBLIC_API}/categories`;
 
-const getCategories = async():Promise<Category[]>=>{
-    const res = await fetch(URL)
-    return res.json();
+const getCategories = async (): Promise<Category[]> => {
+  const res = await fetch(URL);
 
-}
+  return res.json();
+};
 
-const getCategoryWithSlug = async(slug:string):Promise<Category>=>{
-    const res = await fetch(`${URL}/${slug}`);
+const getCategoryWithSlug = async (slug: string): Promise<Category> => {
+  const res = await fetch(`${URL}/${slug}`);
 
-    return res.json();
+  return res.json();
+};
 
-}
-
-
-
-export {getCategories,getCategoryWithSlug}
-
+export { getCategories, getCategoryWithSlug };
