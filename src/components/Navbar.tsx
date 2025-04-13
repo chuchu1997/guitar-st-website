bd693111910f962314218b4fc872b1fe7a6c8309
cuong <tuilanguyencuong@gmail.com>
2025-04-13 12:17:02 +0700

    add fetch categoriues

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,6 +6,8 @@ import Link from "next/link";
 import MainNavbar from "./MainNavbar";
 import { getCategories } from "@/actions/get-categories";
 import { AppSidebar } from "./app-sidebar";
+export const dynamic = "force-dynamic";
+export const revalidate = 60;
 
 const Navbar = async () => {
   const categories = await getCategories();
